91514d925b4d8e02a573d7c3586e7c6b42ba50df
alkemann <alexander@illustrata.no>
2009-07-21 14:52:01 +0200

    test for Article::publish() and Article::delete()

--- tests/cases/models/article.test.php
+++ tests/cases/models/article.test.php
@@ -54,6 +54,101 @@ class TagTestCase extends CakeTestCase {
 		#debug($result);
 	}
 
+	function testPublish() {
+		$this->loadFixtures('User','Category');
+		$data = array('Article' => array(
+			'title' => 'Test Article',
+			'category_id' => 2,
+			'user_id' => 1
+		));
+		$this->Article->create($data);
+		$this->Article->save();
 
+		/**
+		 *  @todo Should this setting be automatic?
+		 *  ie check if Article publish status = 0 + publised_data = null
+		 */
+		$this->saveDraft = false;
+		$this->Article->Intro->saveDraft = false;
+
+		$result = $this->Article->find('first', array(
+			'recursive' => -1,
+			'fields' => array('id','published','published_date','deleted'),
+			'conditions' => array('id' => 1)));
+		$this->assertFalse($result['Article']['published'], 'Article started out published : %S');
+		$this->assertNull($result['Article']['published_date'], 'Article started out with published date : %S');
+		$this->assertFalse($result['Article']['deleted'], 'Article started out deleted : %S');
+
+		$this->assertTrue($this->Article->publish(1), 'Failed to publish Article : %s');
+
+		$result = $this->Article->find('first', array(
+			'recursive' => -1,
+			'fields' => array('id','published','published_date','deleted'),
+			'conditions' => array('id' => 1)));
+		$this->assertTrue($result['Article']['published'], 'Article did not get published : %S');
+		$this->assertNotNull($result['Article']['published_date'], 'Article did not get published date : %S');
+		$this->assertFalse($result['Article']['deleted'], 'Article got deleted : %S');
+	}
+	
+	function testDelete() {
+		$this->loadFixtures('Article','ArticlePage');
+
+		$this->Article->Behaviors->attach('Containable');
+		$result = $this->Article->find('first', array(
+			'contain' => array('ArticlePage','Intro'),
+			'fields' => array('Article.id','Article.published','Article.published_date','Article.deleted','Article.deleted_date',
+				'Intro.id','Intro.article_id','Intro.content'),
+			'conditions' => array('Article.id' => 1)
+		));
+
+		$this->assertTrue($result['Article']['published'], 'Article isnt published : %S');
+		$this->assertNotNull($result['Article']['published_date'], 'Article does not have published date : %S');
+		$this->assertFalse($result['Article']['deleted'], 'Article is deleted : %S');
+		$this->assertNull($result['Article']['deleted_date'], 'Article has deleted date : %S');
+		$this->assertFalse(empty($result['ArticlePage']), 'Article has no pages : %S');
+		$this->assertFalse(empty($result['Intro']), 'Article has no Intro : %S');
+
+		$this->assertTrue($this->Article->delete(1), 'Failed to delete article : %s');
+
+		$result = $this->Article->find('first', array(
+			'contain' => array('ArticlePage','Intro'),
+			'fields' => array('Article.id','Article.published','Article.published_date','Article.deleted','Article.deleted_date',
+				'Intro.id','Intro.article_id','Intro.content'),
+			'conditions' => array('Article.id' => 1)
+		));
+
+		$this->assertFalse($result['Article']['published'], 'Article is published : %S');
+		$this->assertNotNull($result['Article']['published_date'], 'Article does not have published date : %S');
+		$this->assertTrue($result['Article']['deleted'], 'Article isn\'t deleted : %S');
+		$this->assertNotNull($result['Article']['deleted_date'], 'Article does not have deleted date : %S');
+		$this->assertFalse(empty($result['ArticlePage']), 'Article has no pages : %S');
+		$this->assertFalse(empty($result['Intro']), 'Article has no Intro : %S');
+
+	}
+
+	function testPublishFlow() {
+		$this->loadFixtures('User','Category');
+		$data = array('Article' => array(
+			'title' => 'Test Article',
+			'category_id' => 2,
+			'user_id' => 1
+		));
+		$this->Article->create($data);
+		$this->Article->save();
+
+		/**
+		 *  @todo Should this setting be automatic?
+		 *  ie check if Article publish status = 0 + publised_data = null
+		 */
+		$this->saveDraft = false;
+		$this->Article->Intro->saveDraft = false;
+
+		$this->assertFalse($this->Article->published(1), 'Article started out published : %S');
+
+		$this->assertTrue($this->Article->publish(1), 'Failed to publish Article : %s');
+
+		$this->assertTrue($this->Article->published(1), 'Article did not get published : %S');
+
+	}
 }
 ?>
