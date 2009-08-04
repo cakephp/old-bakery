ea66f4b4542c29251399893f3862e29c4ee0ad07
Phally <info@frankdegraaf.net>
2009-08-04 13:43:12 +0200

    Implemented member list.

--- plugins/users/tests/cases/controllers/users_controller.test.php
+++ plugins/users/tests/cases/controllers/users_controller.test.php
@@ -29,29 +29,33 @@ class TestUsersController extends UsersController {
 class UsersControllerTestCase extends CakeTestCase {
 	private $Users = null;
 	public $fixtures = array('plugin.users.user', 'plugin.users.message', 'plugin.users.conversation', 'plugin.users.conversations_user');
+	private $config = 'test_suite_permissions.php';
+	
+	public function startCase() {
+		$testfile = APP . 'plugins' . DS . 'users' . DS . 'tests' . DS . 'files' . DS . 'permissions.php.test';
+		$configfile = APP . 'config' . DS . $this->config;
+		copy($testfile, $configfile);
+	}
 	
 	public function startTest() {
 		$this->Users = new TestUsersController();
 		$this->Users->constructClasses();
-		
-		$this->Users->params = Router::parse('/');
-		
-		$this->Users->Component->initialize($this->Users);
-		
+		$this->Users->Access->file = 'test_suite_permissions';
 	}
 	
 	public function testLoginActionWithValidUser() {
+		$this->Users->params['url']['url'] = Router::normalize($this->Users->params = Router::parse('/users/users/login'));
+		$this->Users->Component->initialize($this->Users);
 		$this->Users->data = array(
 			$this->Users->Auth->userModel => array(
 				$this->Users->Auth->fields['username'] => 'Phally',
-				$this->Users->Auth->fields['password'] => '86a8c2da8527a1c6978bdca6d7986fe14ae147fe',
+				$this->Users->Auth->fields['password'] => 'frank',
 				$this->Users->Access->rememberField => 1
 			)
 		);
 		
 		$this->Users->beforeFilter();
 		$this->Users->Component->startup($this->Users);
-		$this->Users->Auth->login($this->Users->data);
 		$this->Users->login();
 		
 		$result = $this->Users->Access->getRememberCookie();
@@ -59,17 +63,20 @@ class UsersControllerTestCase extends CakeTestCase {
 			$this->Users->Auth->fields['username'] => 'Phally',
 			$this->Users->Auth->fields['password'] => '86a8c2da8527a1c6978bdca6d7986fe14ae147fe',
 		);
-		$this->assertEqual($result, $expected);
+		$this->assertEqual($result, $expected, 'User has been placed in the remember me cookie');
 		
-		$this->assertEqual($this->Users->Auth->user('username'), 'Phally');
+		$this->assertEqual($this->Users->Auth->user('username'), 'Phally', 'User is logged in');
 		
 		if (!$this->Users->Auth->loginRedirect) {
 			$this->Users->Auth->loginRedirect = '/';
 		}
-		$this->assertEqual($this->Users->redirectUrl, $this->Users->Auth->loginRedirect);
+		$this->assertEqual($this->Users->redirectUrl, $this->Users->Auth->loginRedirect, 'User is redirected');
 	}
 	
 	public function testLoginActionWithoutValidUser() {
+		$this->Users->params['url']['url'] = Router::normalize($this->Users->params = Router::parse('/users/users/login'));
+		$this->Users->Component->initialize($this->Users);
+		
 		$this->Users->data = array(
 			$this->Users->Auth->userModel => array(
 				$this->Users->Auth->fields['username'] => 'Phally',
@@ -83,12 +90,14 @@ class UsersControllerTestCase extends CakeTestCase {
 		$this->Users->Auth->login($this->Users->data);
 		$this->Users->login();
 		
-		$this->assertNull($this->Users->Access->getRememberCookie());
-		$this->assertNull($this->Users->Auth->user('username'));
-		$this->assertNull($this->Users->redirectUrl);
+		$this->assertNull($this->Users->Access->getRememberCookie(), 'No remember me cookie is set');
+		$this->assertNull($this->Users->Auth->user('username'), 'No user is logged in');
+		$this->assertNull($this->Users->redirectUrl, 'No redirects took place');
 	}
 	
 	public function testLogoutAction() {
+		$this->Users->params['url']['url'] = Router::normalize($this->Users->params = Router::parse('/users/users/logout'));
+		$this->Users->Component->initialize($this->Users);
 		$this->Users->beforeFilter();
 		
 		$data = array(
@@ -105,9 +114,34 @@ class UsersControllerTestCase extends CakeTestCase {
 		$this->Users->Component->startup($this->Users);
 		$this->Users->logout();
 		
-		$this->assertNull($this->Users->Auth->user());
-		$this->assertNull($this->Users->Access->getRememberCookie());
-		$this->assertEqual($this->Users->redirectUrl, Router::normalize($this->Users->Auth->logoutRedirect));
+		$this->assertNull($this->Users->Auth->user(), 'User is logged out');
+		$this->assertNull($this->Users->Access->getRememberCookie(), 'Remember me cookie has been deleted');
+		$this->assertEqual($this->Users->redirectUrl, Router::normalize($this->Users->Auth->logoutRedirect), 'User is redirected to the logoutRedirect');
+	}
+
+	public function testIndexActionWithoutSearchParameters() {
+		$this->Users->params['url']['url'] = Router::normalize($this->Users->params = Router::parse('/users/users/index'));
+		$this->Users->Component->initialize($this->Users);
+		$this->Users->beforeFilter();
+		
+		$this->Users->Component->startup($this->Users);
+		$this->Users->index();
+		
+		$this->assertEqual(Set::extract('/User/id', $this->Users->viewVars['users']), array(5, 4, 3, 7, 6, 1, 2), 'View variable \'$users\' matches');
+		$this->assertNull($this->Users->redirectUrl, 'No redirects');
+	}
+
+	public function testIndexActionWithSearchParameters() {
+		$url = '/users/users/index/keyword:a';
+		$this->Users->params = array_merge(Router::parse($url), array('url' => array('url' => $url)));
+		
+		$this->Users->Component->initialize($this->Users);
+		$this->Users->beforeFilter();
+		
+		$this->Users->Component->startup($this->Users);
+		$this->Users->index();
+		$this->assertEqual(Set::extract('/User/id', $this->Users->viewVars['users']), array(5, 4, 1), 'View variable \'$users\' matches');
+		$this->assertNull($this->Users->redirectUrl, 'No redirects');
 	}
 	
 	public function endTest() {
@@ -115,6 +149,10 @@ class UsersControllerTestCase extends CakeTestCase {
 		$this->Users->Access->Cookie->destroy();
 		unset($this->Users);
 		ClassRegistry::flush();
+	}
+	
+	public function endCase() {
+		@unlink(APP . 'config' . DS . $this->config);
 	}
 }
 
---
+++ views/elements/paging.ctp
@@ -0,0 +1,1 @@
+<?php echo $paginator->prev(); ?> <?php echo $paginator->numbers(array('modulus' => 2, 'first' => 2, 'last' => 2)); ?> <?php echo $paginator->next(); ?>
