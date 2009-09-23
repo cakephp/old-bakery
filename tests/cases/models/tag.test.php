86e61bda07fddcb47aaf60504bea838dd6209427
alkemann <alexander@illustrata.no>
2009-09-23 13:50:17 +0200

    added test for saving tags along with articles

--- tests/cases/models/tag.test.php
+++ tests/cases/models/tag.test.php
@@ -48,6 +48,59 @@ class TagTestCase extends CakeTestCase {
 		$result = $this->Tag->saveArticleTags('Love,New,Php');
 		$expected = array(2,4,5);
 		$this->assertEqual($result,$expected);
+
+		$result = $this->Tag->saveArticleTags('Love,fun,New,Php');
+		$expected = array(1,2,4,5);
+		$this->assertEqual($result,$expected);
+	}
+
+	function testSaveArticleWithTags() {
+		$this->loadFixtures('Article','ArticlePage','ArticlesTag','Tag');
+		$this->Tag->Behaviors->attach('Containable');
+		$this->Tag->Article->Behaviors->attach('Containable');
+		$tags = $this->Tag->saveArticleTags('Stuff,Joy,News');
+		if ($this->skipIf(
+				empty($tags) ||
+				!is_array($tags) ||
+				sizeof($tags) != 3 ||
+				$tags[0] != 1 ||
+				$tags[1] != 3 ||
+				$tags[2] != 4,
+			'saveArticleTags() does not work')) return;
+
+		$articleData = array(
+			'Article' => array(
+				'parent_id' => 1,
+				'title' => 'A tagged article',
+				'category_id' => 1,
+				'user_id' => 1
+			),
+			'Intro' => array(
+				'content' => 'Intro to a tag system'
+			),
+			'Tag' => array(
+				'Tag' => $tags
+			)
+		);		
+		$this->Tag->Article->create($articleData);
+		$this->Tag->Article->Intro->saveDraft = false;
+		$this->Tag->Article->ArticlePage->saveDraft = false;
+		$this->assertTrue($this->Tag->Article->saveAll());
+
+		$this->Tag->Article->contain('Intro','Tag');
+		$result = $this->Tag->Article->read();
+		$this->assertFalse(empty($result));
+		$this->assertEqual(2, $result['Article']['id']);
+		$this->assertEqual('A tagged article', $result['Article']['title']);
+		$this->assertEqual('Intro to a tag system', $result['Intro']['content']);
+		$this->assertEqual(3, sizeof($result['Tag']), 'Not enough tag saved : %s');
+		if ($this->skipIf(3 != sizeof($result['Tag']), 'Not enough tag saved'));
+		$this->assertEqual(1,$result['Tag'][0]['id']);
+		$this->assertEqual('News',$result['Tag'][0]['name']);
+		$this->assertEqual(3,$result['Tag'][1]['id']);
+		$this->assertEqual('Stuff',$result['Tag'][1]['name']);
+		$this->assertEqual(4,$result['Tag'][2]['id']);
+		$this->assertEqual('Joy',$result['Tag'][2]['name']);
 	}
 
 }
--- tests/fixtures/article_page_fixture.php
+++ tests/fixtures/article_page_fixture.php
@@ -23,7 +23,7 @@ class ArticlePageFixture extends CakeTestFixture {
 		array(
 			'id'  => 2,
 			'article_id'  => 1,
-			'title'  => 'Intro : Lorem ipsum dolor sit amet',
+			'title'  => null,
 			'page_number'  => 0,
 			'content'  => 'Lorem ipsum dolor sit amet, aliquet feugiat. Convallis morbi fringilla gravida, phasellus feugiat dapibus velit nunc, pulvinar eget sollicitudin venenatis cum nullam, vivamus ut a sed, mollitia lectus. Nulla vestibulum massa neque ut et, id hendrerit sit, feugiat in taciti enim proin nibh, tempor dignissim, rhoncus duis vestibulum nunc mattis convallis.'
 		),
